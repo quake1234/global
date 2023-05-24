f11473fb2e570706d0b080bde489327685309822
max <maxpax92@gmail.com>
2023-05-24 23:17:11 +0200

    first commit

---
+++ jooq/src/main/java/com/starterproject/Jooq.java
@@ -0,0 +1,12 @@
+package com.starterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jooq {
+    public static void main(String[] args) {
+        SpringApplication.run(Jooq.class, args);
+    }
+
+}
